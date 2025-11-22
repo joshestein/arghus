23b8bfa6425aa28e9da9846932070d47980d9495
Josh Stein <josh.e.stein@gmail.com>
2025-11-22 14:23:32 +0200

    Setup supabase react native connection

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { supabase } from "./lib/supabase";
 
 export default function App() {
+  useEffect(() => {
+    const fetchState = async () => {
+      const { data } = await supabase.from("active_calls").select("*").eq("id", 1).single();
+
+      if (data) {
+        console.log(data);
+      }
+    };
+
+    fetchState();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
