7f2a3052a5eeb4760773739be2e03b26d99f57e4
Josh Stein <josh.e.stein@gmail.com>
2025-11-23 13:13:06 +0200

    Simplify client

--- mobile/lib/supabase.ts
+++ mobile/lib/supabase.ts
@@ -3,10 +3,4 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
 const supabasePublishableKey = process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
 
-export const supabase = createClient(supabaseUrl, supabasePublishableKey, {
-  auth: {
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: false,
-  },
-});
+export const supabase = createClient(supabaseUrl, supabasePublishableKey);
